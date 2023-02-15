acf58618025953e427e73ae37308ff638d7844df
Bálint <bbalint0404@gmail.com>
2023-02-15 10:34:04 +0100

    Add project files.

---
+++ ConwayBusinessLogic/PlayArea.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwayBusinessLogic
+{
+    public class PlayArea //internal = namespace-en belül látható
+    {
+        public int Cols { get; private set; }
+        public int Rows { get; private set; }
+        public int Cells { get; private set; }
+        bool[,] Map;
+        public PlayArea(int cols, int rows, int cells)
+        {
+            Cols = cols;
+            Rows = rows;
+            Cells = cells;
+            Map = new bool[rows, cols];
+        }
+
+        private void Randomize()
+        {
+            Random rnd = new Random();
+            var elements = Enumerable.Range(0, Cols*Rows);
+            rnd.Next(0, elements.Count());
+            element
+        }
+        public void DrawMap()
+        {
+            for (int i = 0; i < Cols; i++){
+
+            }
+        }
+    }
+}
---
+++ ConwayConsolePresentation/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using ConwayBusinessLogic;
+
+namespace ConwayConsolePresentation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------========== Conway's Game of Life ==========----------");
+            string colsString, rowsString, cellsString;
+            //do
+            //{
+            Console.Write("Sorok száma: ");
+            colsString = Console.ReadLine();
+            //} while (!Int32.TryParse(colsString, out int cols));
+            //do
+            //{
+            Console.Write("Oszlopok száma: ");
+            rowsString = Console.ReadLine();
+            //} while (!Int32.TryParse(rowsString, out int rows));
+            //do
+            //{
+            Console.Write("Elemek száma: ");
+            cellsString = Console.ReadLine();
+            //} while (!Int32.TryParse(elementsString, out int elements));
+
+            if (Int32.TryParse(colsString, out int cols) && Int32.TryParse(rowsString, out int rows) && Int32.TryParse(cellsString, out int cells))
+            {
+                PlayArea playArea = new PlayArea(cols, rows, cells);
+            }
+            else
+            {
+                Console.WriteLine("Valami nem jó");
+            }
+
+            //try //lassú az Exception obj. miatt
+            //{
+            //    var cols = Convert.ToInt32(colsString);
+            //    var rows = Convert.ToInt32(rowsString);
+            //    var elements = Convert.ToInt32(elementsString);
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("Nem megfelelő értékek!");
+            //    return;
+            //}
+
+            Console.WriteLine("Done.");
+            Console.ReadKey();
+        }
+    }
+}
